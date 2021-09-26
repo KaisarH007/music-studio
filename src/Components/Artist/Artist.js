855d1879f34cda4367df2efd1316951536b801a1
KaisarH007 <kaisarh007@gamil.com>
2021-09-26 19:11:11 +0600

    modify

--- src/Components/Artist/Artist.js
+++ src/Components/Artist/Artist.js
@@ -7,7 +7,7 @@ const Artist = (props) => {
     return (
         <div className="col-md-4">
            
-           <div class="card h-100 card-bg text-color">
+           <div class="card h-100 card-bg tex-black rounded">
       <img style={{height:"150px" ,width:"150px"}} src={img} class="card-img-top rounded-circle  rounded mx-auto d-block p-2 " alt="..."/>
       <div class="card-body">
         <h5 class="card-title text-center fw-bold">{name}</h5>
@@ -15,7 +15,7 @@ const Artist = (props) => {
             <h5>Occupation : <small>{occupation}</small></h5>
             <h5>Age: <small>{born}</small></h5>
             <h5>Nationality: <small>{country}</small></h5>
-            <h5>Salary: <small>{slary}</small></h5>
+            <h5>Salary: ৳ <small>{slary}</small></h5>
             <button onClick={()=>props.handleAddArtist(props.artist)} type="button" class="btn text-white bg-danger">Click to Hired</button>
 
         </div>
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+const icon = <FontAwesomeIcon icon={faCoffee} />
+
 
 const Cart = (props) => {
 const artists = props.cartArtist
@@ -11,27 +15,27 @@ const totalCost = artists.reduce(totalCostRuducer  , 0)
 
     return (
         <div className="text-center">
-            <h2 className="fw- text-white">Total Hired :{artists.length}</h2>
-            <h2 className="fw- text-white">Total Cost : {totalCost}</h2>
+            <h2 className="fw- text-white">{icon}Total Hired :{artists.length}</h2>
+            <h2 className="fw- text-white">Total Cost: ৳ {totalCost}</h2>
 
               <h4 class="text-warning fw-bold">Hireded Artists</h4>
                 
             <ul>
                 {
                 artists.map(artist=>
-                <div class="card " style={{"width":"18rem"}}>
+                <div class="card bg-danger" style={{"width":"18rem"}}>
             
-                <ul class="list-group list-group-flush">
+                <ul class="list-group list-group-flush ">
                 
-                  <li class="list-group-item bg-warning">
+                  <li class="list-group-item ">
                       <div><img style={{height:"50px" ,width:"50px"}} src={artist.img} alt="" />
                       </div>
                       <div>{artist.name}</div></li>
-                 
-                  
                 </ul>
+               
               </div>)
                 }
+                 <button className="btn bg-danger text-white mt-2 fw-bold">Confirm</button>
             </ul>
         </div>
 
